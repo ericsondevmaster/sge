e579f0fd7f7eaf62431270f60fdfde3322ec8e25
ericsondevmaster <ericson.devmaster@gmail.com>
2024-07-08 13:23:11 -0300

    Calculating product metrics

--- app/views.py
+++ app/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
+from . import metrics
 
 
 def home(request):
-    product_metrics = {
-        'total_cost_price': 500000,
-        'total_selling_price': 1500000,
-        'total_quantity': 5000,
-        'total_profit': 1000000
-    }
+    product_metrics = metrics.get_product_metrics()
 
     context = {
         'product_metrics': product_metrics
