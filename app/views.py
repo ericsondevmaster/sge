7b00d15418b2773a4948fc7e46775ac459515a3c
ericsondevmaster <ericson.devmaster@gmail.com>
2024-07-08 12:51:50 -0300

    Product metrics in context

--- app/views.py
+++ app/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 
 def home(request):
-    context = {}
+    product_metrics = {
+        'total_cost_price': 500000,
+        'total_selling_price': 1500000,
+        'total_quantity': 5000,
+        'total_profit': 1000000
+    }
+
+    context = {
+        'product_metrics': product_metrics
+    }
+
     return render(request, 'home.html', context)
